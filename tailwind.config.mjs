9b06fded076ee0978dc5bb7840b3512f437ab6dd
orangeletters <orangeletters@proton.me>
2025-02-22 17:35:58 -0500

    empezando de nuevo la practica

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend:
+		{
+			keyframes:
+			{
+				'rainbow':
+				{
+					'0%': {color: 'red'},
+					'25%': {color: 'blue'},
+					'35%': {color: 'gold'},
+					'45%': {color: 'lime'},
+					'55%': {color: 'green'},
+					'65%': {color: 'cyan'},
+					'75%': {color: 'purple'},
+					'85%': {color: 'pink'},
+					'95%': {color: 'hotpink'},
+					'100%': {color: 'red'},
+				},
+			},
+			animation:{
+				'rainbow': 'rainbow 3s linear infinite',
+			}
+		},
 	},
 	plugins: [],
-}
+};
